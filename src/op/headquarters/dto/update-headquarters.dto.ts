2e45da642e11be37919da92170315c5af206ea41
EscAdn <adan.imh@gmail.com>
2025-07-01 17:34:23 -0600

    OP-MS > Delete Project in update headquarters

--- src/op/headquarters/dto/update-headquarters.dto.ts
+++ src/op/headquarters/dto/update-headquarters.dto.ts
@@ -1,5 +1,7 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 
 import { CreateHeadquartersDto } from './create-headquarters.dto';
 
-export class UpdateHeadquartersDto extends PartialType(CreateHeadquartersDto) {}
+export class UpdateHeadquartersDto extends PartialType(
+  OmitType(CreateHeadquartersDto, ['project'] as const),
+) {}
