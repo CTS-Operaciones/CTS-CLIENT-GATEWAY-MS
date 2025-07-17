766f14091f45f9123d0830fa8566dd1da8ef5a28
DesarrolloCTS <desarrollo@centrodetecnologias.mx>
2025-07-16 18:01:38 -0600

    add inventoryHasAdd

--- src/saga/add-remove/add-remove.controller.ts
+++ src/saga/add-remove/add-remove.controller.ts
@@ -3,12 +3,12 @@ import { ClientProxy } from '@nestjs/microservices';
 import { ApiTags } from '@nestjs/swagger';
 import { NATS_SERVICE, sendAndHandleRpcExceptionPromise } from 'src/common';
 import { CreateAddRemoveDto } from './dto/create-add-remove.dto';
-
+import { CreateHasAddRemoveDto } from '../add-remove/dto/create-inventory-has-add-remove.dto';
 @ApiTags('add-remove')
 @Controller('add-remove')
 export class AddRemoveController {
   constructor(
-    @Inject(NATS_SERVICE) private readonly clientAddRemove: ClientProxy
+    @Inject(NATS_SERVICE) private readonly clientAddRemove: ClientProxy,
   ) {}
 
   @Post()
@@ -16,8 +16,8 @@ export class AddRemoveController {
     return await sendAndHandleRpcExceptionPromise(
       this.clientAddRemove,
       'createAddRemove',
-      createAddRemoveDto
-    )
+      createAddRemoveDto,
+    );
   }
 
   @Get()
@@ -25,26 +25,29 @@ export class AddRemoveController {
     return await sendAndHandleRpcExceptionPromise(
       this.clientAddRemove,
       'findAllAddRemove',
-      {}
-    )
+      {},
+    );
   }
 
   @Get(':id')
   async getResource(@Param('id', ParseIntPipe) id: number) {
-      return await sendAndHandleRpcExceptionPromise(
-        this.clientAddRemove,
-        'findOneAddRemove',
-        { id }
-      )
-    }
+    return await sendAndHandleRpcExceptionPromise(
+      this.clientAddRemove,
+      'findOneAddRemove',
+      { id },
+    );
+  }
 
   @Patch()
-  async updateAddRemove(@Param('id', ParseIntPipe) id: number, @Body() updateAddRemoveDto: CreateAddRemoveDto) {
+  async updateAddRemove(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateAddRemoveDto: CreateAddRemoveDto,
+  ) {
     return await sendAndHandleRpcExceptionPromise(
       this.clientAddRemove,
       'updateAddRemove',
-      { id, ...updateAddRemoveDto }
-    )
+      { id, ...updateAddRemoveDto },
+    );
   }
 
   @Delete(':id')
@@ -52,7 +55,38 @@ export class AddRemoveController {
     return await sendAndHandleRpcExceptionPromise(
       this.clientAddRemove,
       'removeAddRemove',
-      { id }
-    )
+      { id },
+    );
   }
 }
+
+export class inventoryHasAddRemoveController {
+  @Inject(NATS_SERVICE) private readonly clientInventoryhasAdd: ClientProxy;
+  @Post()
+  async addRemove(@Body() createInventoryhasAdd: CreateHasAddRemoveDto) {
+    return await sendAndHandleRpcExceptionPromise(
+      this.clientInventoryhasAdd,
+      'createInventoryHasAdd',
+      createInventoryhasAdd,
+    );
+  }
+
+  @Get()
+  async findAll() {
+    return await sendAndHandleRpcExceptionPromise(
+      this.clientInventoryhasAdd,
+      'findOneInventoryHasAdd',
+      {},
+    );
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return await sendAndHandleRpcExceptionPromise(
+      this.clientInventoryhasAdd,
+      'removeInventoryHasAdd',
+      { id },
+    );
+  }
+}
+  
